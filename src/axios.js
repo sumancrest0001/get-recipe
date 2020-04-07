2a44f9a42e14b0bef5a21030563f4510a77e9322
sumancrest0001 <suman.crest0001@gmail.com>
2020-04-07 22:04:29 +0545

    Removed yarn.lock file

--- src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import key from './config';
-
-const instance = axios.create({
-  baseURL: `https://api.spoonacular.com/recipes/random?number=15&apiKey=${key}`,
-});
-
-export default instance;
